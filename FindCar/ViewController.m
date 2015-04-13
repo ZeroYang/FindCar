018463caa4526558d41a07e925a7f59d91e94287
Yangtb <731111475@qq.com>
2015-04-13 22:43:58 +0800

    create button for get location

--- FindCar/ViewController.m
+++ FindCar/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    CLLocationManager *locManager;
+}
 
 @end
 
@@ -17,11 +20,90 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(100, 100, 50, 100)];
+    [btn setTitle:@"定位" forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor grayColor] forState:UIControlStateHighlighted];
+    [btn addTarget:self action:@selector(getMyCarLocation) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+    
+    
+    
+    UIButton *btn1 = [[UIButton alloc] initWithFrame:CGRectMake(100, 200, 50, 100)];
+    [btn1 setTitle:@"找车" forState:UIControlStateNormal];
+    [btn1 setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [btn1 setTitleColor:[UIColor grayColor] forState:UIControlStateHighlighted];
+    [btn1 addTarget:self action:@selector(getMyCarLocation) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn1];
+    
+    [self.view setBackgroundColor:[UIColor blueColor]];
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)getMyCarLocation
+{
+    NSLog(@"卧槽");
+    //初始化位置管理器
+    locManager = [[CLLocationManager alloc]init];
+    //设置代理
+    locManager.delegate = self;
+    //设置位置经度
+    locManager.desiredAccuracy = kCLLocationAccuracyBest;
+    //设置每隔100米更新位置
+    locManager.distanceFilter = 50;
+    //开始定位服务
+    [locManager startUpdatingLocation];
+    
+}
+
+- (void)findMyCar
+{
+    
+}
+
+//协议中的方法，作用是每当位置发生更新时会调用的委托方法
+-(void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation
+{
+    //停止定位服务
+    [locManager stopUpdatingLocation];
+    
+    //结构体，存储位置坐标
+    CLLocationCoordinate2D loc = [newLocation coordinate];
+    float longitude = loc.longitude;
+    float latitude = loc.latitude;
+    //self.lonLabel.text = [NSStringstringWithFormat:@"%f",longitude];
+    //self.latLabel.text = [NSStringstringWithFormat:@"%f",latitude];
+    
+    NSString *location = [NSString stringWithFormat:@"%f,%f",longitude,latitude];
+
+    
+    UIAlertView *alertView = [[UIAlertView alloc]initWithTitle:@"Location"
+                                                       message:location delegate:self cancelButtonTitle:@"Ok"otherButtonTitles:nil, nil];
+    [alertView show];
+
+}
+
+//当位置获取或更新失败会调用的方法
+-(void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+    NSString *errorMsg = nil;
+    if ([error code] == kCLErrorDenied) {
+        errorMsg = @"访问被拒绝";
+    }
+    if ([error code] == kCLErrorLocationUnknown) {
+        errorMsg = @"获取位置信息失败";
+    }
+    
+    UIAlertView *alertView = [[UIAlertView alloc]initWithTitle:@"Location"
+                                                      message:errorMsg delegate:self cancelButtonTitle:@"Ok"otherButtonTitles:nil, nil];
+    [alertView show];
+    //[alertView release];
 }
 
 @end
